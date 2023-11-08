b22e239ea71a4ef54c43b0f7a1793288a04bc560
Yahav <yahav.shasha@gmail.com>
2023-11-08 21:22:39 +0200

    fix exception when using redis adapter and metrics disabled

--- src/metrics/metrics.ts
+++ src/metrics/metrics.ts
@@ -71,35 +71,45 @@ export class Metrics implements MetricsInterface {
      * Track the time in which horizontal adapter resolves requests from other nodes.
      */
     trackHorizontalAdapterResolveTime(appId: string, time: number): void {
-        this.driver.trackHorizontalAdapterResolveTime(appId, time);
+        if (this.server.options.metrics.enabled) {
+            this.driver.trackHorizontalAdapterResolveTime(appId, time);
+        }
     }
 
     /**
      * Track the fulfillings in which horizontal adapter resolves requests from other nodes.
      */
     trackHorizontalAdapterResolvedPromises(appId: string, resolved = true): void {
-        this.driver.trackHorizontalAdapterResolvedPromises(appId, resolved);
+        if (this.server.options.metrics.enabled) {
+            this.driver.trackHorizontalAdapterResolvedPromises(appId, resolved);
+        }
     }
 
     /**
      * Handle a new horizontal adapter request sent.
      */
     markHorizontalAdapterRequestSent(appId: string): void {
-        this.driver.markHorizontalAdapterRequestSent(appId);
+        if (this.server.options.metrics.enabled) {
+            this.driver.markHorizontalAdapterRequestSent(appId);
+        }
     }
 
      /**
       * Handle a new horizontal adapter request that was marked as received.
       */
     markHorizontalAdapterRequestReceived(appId: string): void {
+         if (this.server.options.metrics.enabled) {
         this.driver.markHorizontalAdapterRequestReceived(appId);
+         }
     }
 
      /**
       * Handle a new horizontal adapter response from other node.
       */
     markHorizontalAdapterResponseReceived(appId: string): void {
-        this.driver.markHorizontalAdapterResponseReceived(appId);
+         if (this.server.options.metrics.enabled) {
+             this.driver.markHorizontalAdapterResponseReceived(appId);
+         }
     }
 
     /**
